b7da552fa0cbcc60911b727eaa7ae3fc14c612bc
Steve Gargolinski <steve.gargolinski@gmail.com>
2016-01-18 20:12:10 -0500

    Shows some information.

--- popup.js
+++ popup.js
@@ -1,11 +1,20 @@
 
+info = ['Here is some information.',
+        'This is additional information.',
+        'Even more information.',
+        'This is even more additional information.',
+        'You have exhausted the information.',
+];
+
 document.addEventListener('DOMContentLoaded', function() {
     chrome.storage.local.get('level', function(result) {
         if (typeof result.level === 'undefined') {
             chrome.storage.local.set({'level': 0});
+        } else {
+            chrome.storage.local.set({'level': result.level + 1});
         }
 
-        chrome.storage.local.set({'level': result.level + 1});
-        document.getElementById('information').textContent = result.level;
+        infoIndex = Math.min(result.level, info.length - 1);
+        document.getElementById('information').textContent = info[infoIndex];
     });
 });
